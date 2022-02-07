5dae11faebd7120a9309f62b6fd280e0dece658f
Cade <zhigaogeng.johnson@gmail.com>
2022-02-07 21:31:09 +0800

    create article; view article

--- article/urls.py
+++ article/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
+    # 文章列表
     path('article-list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ArticlePost
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
+import markdown
 
 # Create your views here.
 
@@ -10,3 +13,36 @@ def article_list(request):
     # 需要传给模板的对象
     context = {'articles':articles}
     return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    # 取出对应id的文章
+    article = ArticlePost.objects.get(id=id)
+
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body, 
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+    # 需要传递给模板的对象
+    context = {'article':article}
+    return render(request, 'article/detail.html', context)
+
+# 写入文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = { 'article_post_form': article_post_form }
+        return render(request, 'article/create.html', context)
+
